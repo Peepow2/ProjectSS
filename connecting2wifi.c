3e83dc5d8c346a3348dd0c50ff9b6eeda0c2c971
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2022-10-23 12:31:48 +0700

    Create connecting2wifi.c

---
+++ connecting2wifi.c
@@ -0,0 +1,17 @@
+void Conecting2wifi()
+{
+    char *SSID = "ชื่อ wifi";
+    char *PASSWORD = "รหัสผ่าน";
+    
+    Serial.begin(115200) // 9600
+    Serial.println("Start");
+    WIFI.begin(SSID, PASSWORD);
+    while (WIFI.status() != WL_CONNECTED)
+    {
+      delay(300); Serial.print(".");
+    }
+    Serial.println()
+    Serial.println("WIFI Connected");
+    Serial.print("IP address = ");
+    Serial.println(WiFi.localIP());
+}
